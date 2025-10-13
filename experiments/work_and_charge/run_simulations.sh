2fb350386337e2652a12df5f7ddde2b865635f3d
Genki Miyauchi <15382014+genkimiyauchi@users.noreply.github.com>
2025-10-13 14:36:58 +0100

    Cleaned experiment script

--- experiments/work_and_charge/run_simulations.sh
+++ experiments/work_and_charge/run_simulations.sh
@@ -7,15 +7,7 @@ STRATEGY_TYPE="$1"
 
 if [ "$STRATEGY_TYPE" = "fixed" ]; then
     STRATEGY_NAME=fixed_charger
-    LINE_SEED=11
-    LINE_TRIAL_DIR=99
-    LINE_CHARGER_FULL_CAPACITY=115
-    LINE_CHARGER_START_CAPACITY=116
-    LINE_IDLE_ENERGY=117
-    LINE_WORK_ENERGY=121
-    LINE_RECHARGE_PER_STEP=122
-    LINE_TRANSFER_LOSS=123
-    LINE_NUM_CHARGERS=129
+    
     NUM_CHARGERS=(0)
     CHARGER_CAPACITY=(0)
 
@@ -32,15 +24,6 @@ if [ "$STRATEGY_TYPE" = "fixed" ]; then
 
 elif [ "$STRATEGY_TYPE" = "mobile" ]; then
     STRATEGY_NAME=mobile_charger
-    LINE_SEED=11
-    LINE_TRIAL_DIR=99
-    LINE_CHARGER_FULL_CAPACITY=115
-    LINE_CHARGER_START_CAPACITY=116
-    LINE_IDLE_ENERGY=117
-    LINE_WORK_ENERGY=121
-    LINE_RECHARGE_PER_STEP=122
-    LINE_TRANSFER_LOSS=123
-    LINE_NUM_CHARGERS=129
 
     # number of chargers
     # NUM_CHARGERS=(1 2 4 6 8 10 12)
@@ -66,10 +49,13 @@ else
     exit 1
 fi
 
-# ACTION NEEDED: Replace '/path/to/folder' to the path to the experiment directory
-EXPERIMENT_DIR=/path/to/folder/swarm-energy-replenishment_prep/experiments/$EXPERIMENT_NAME
-RESULT_DIR=/path/to/folder/swarm-energy-replenishment_prep/results/$EXPERIMENT_NAME
+# Use current working directory as base (the directory from which the script is called)
+BASE_DIR="$(pwd)"
+EXPERIMENT_DIR="$BASE_DIR/experiments/$EXPERIMENT_NAME"
+RESULT_DIR="$BASE_DIR/results/$EXPERIMENT_NAME"
 TEMPLATE_EXPERIMENT_FILE=$EXPERIMENT_DIR/${STRATEGY_NAME}.argos
+
+MAX_SPEED=12.0
 
 # DEFAULT_DELTA_IDLE=0.0
 DEFAULT_DELTA_IDLE=0.005
@@ -126,8 +112,11 @@ do
                         cp $TEMPLATE_EXPERIMENT_FILE $EXPERIMENT_FILE
 
                         # Set seed
-                        let seed=876*k
-                        sed -i "${LINE_SEED}s|.*|random_seed='$seed' />|" $EXPERIMENT_FILE
+                        let SEED=876*k
+                        sed -i "s/random_seed=['\"][0-9]*['\"]/random_seed='$SEED'/" "$EXPERIMENT_FILE"
+
+                        # Update max_speed
+                        sed -i "s/max_speed=\"[0-9.]*\"/max_speed=\"$MAX_SPEED\"/" "$EXPERIMENT_FILE"
 
                         # # Set run number
                         # sed -i "${LINE_RUN_NUMBER}s/.*/run_number='$k'/" $EXPERIMENT_FILE
@@ -135,49 +124,49 @@ do
                         echo $TRIAL_DIR_PATH
 
                         # Set trial directory path
-                        sed -i "${LINE_TRIAL_DIR}s|.*|out_path='results/${EXPERIMENT_NAME}/${TRIAL_DIR}/'|" $EXPERIMENT_FILE
+                        sed -i "s|out_path=.*|out_path='results/${EXPERIMENT_NAME}/${TRIAL_DIR}/'|" $EXPERIMENT_FILE
 
                         # Set idle energy
-                        sed -i "${LINE_IDLE_ENERGY}s|.*|delta_time='$DEFAULT_DELTA_IDLE'|" $EXPERIMENT_FILE
+                        sed -i "s/delta_time=\"[0-9.]*\"/delta_time=\"$DEFAULT_DELTA_IDLE\"/" "$EXPERIMENT_FILE"
 
                         # Set work energy
                         rate=$(echo "$DEFAULT_DELTA_WORK*$j" | bc -l)
                         rate=$(printf "%.3f" $rate)
-                        sed -i "${LINE_WORK_ENERGY}s|.*|<extra_battery_info delta_work='$rate'|" $EXPERIMENT_FILE
+                        sed -i "s/<extra_battery_info delta_work=\"[0-9.]*\"/<extra_battery_info delta_work=\"$rate\"/" "$EXPERIMENT_FILE"
 
                         # Set recharge per step
                         rate=$(echo "$DEFAULT_DELTA_CHARGE*$m" | bc -l)
                         rate=$(printf "%.3f" $rate)
-                        sed -i "${LINE_RECHARGE_PER_STEP}s|.*|delta_recharge='$rate'|" $EXPERIMENT_FILE
+                        sed -i "s/delta_recharge=\"[0-9.]*\"/delta_recharge=\"$rate\"/" "$EXPERIMENT_FILE"
 
                         # Set transfer loss
                         rate=$(echo "$DEFAULT_DELTA_TRANSFER_LOSS*$o" | bc -l)
                         rate=$(printf "%.3f" $rate)
-                        sed -i "${LINE_TRANSFER_LOSS}s|.*|delta_transfer_loss='$o'/>|" $EXPERIMENT_FILE
+                        sed -i "s/delta_transfer_loss=\"[0-9.]*\"/delta_transfer_loss=\"$rate\"/" "$EXPERIMENT_FILE"
 
                         if [ "$STRATEGY_TYPE" = "mobile" ]; then
                             # # Set charger move energy
                             # sed -i "${LINE_CHARGER_MOVE_ENERGY}s|.*|delta_pos_charger='$rate'/>|" $EXPERIMENT_FILE
 
                             # Set charger full capacity
-                            sed -i "${LINE_CHARGER_FULL_CAPACITY}s|.*|full_charge_charger='$i'|" $EXPERIMENT_FILE
+                            sed -i "s/full_charge_charger=\"[0-9]*\"/full_charge_charger=\"$i\"/" $EXPERIMENT_FILE
 
                             # Set charger start capacity
-                            sed -i "${LINE_CHARGER_START_CAPACITY}s|.*|start_charge_charger='$i,$i'|" $EXPERIMENT_FILE
+                            sed -i "s/start_charge_charger=\"[0-9]*,[0-9]*\"/start_charge_charger=\"$i,$i\"/" $EXPERIMENT_FILE
 
                             # Set number of chargers
-                            sed -i "${LINE_NUM_CHARGERS}s|.*|<e-puck_charger controller='charger' num_robots='$l'/>|" $EXPERIMENT_FILE
+                            sed -i "s|<e-puck_charger controller='charger' num_robots='[0-9]*'/>|<e-puck_charger controller='charger' num_robots='$l'/>|" "$EXPERIMENT_FILE"
                         fi
 
                         # Run experiment
-                        echo "Running experiment $count, t_loss = $o, charge_rate = $m, work_rate = $j, num_chargers = $l, capacity = $i, run = $k, (s = $seed) ..."
+                        echo "Running experiment $count, t_loss = $o, charge_rate = $m, work_rate = $j, num_chargers = $l, capacity = $i, run = $k, (s = $SEED) ..."
                         # LOG_FILE="$RESULT_DIR/log.txt"
                         # LOG_ERR_FILE="$RESULT_DIR/log_err.txt"
 
                         # Set log files to be discarded when running argos3
                         LOG_FILE="/dev/null"
                         LOG_ERR_FILE="/dev/null"
-                        argos3 -c $EXPERIMENT_FILE -l $LOG_FILE -e $LOG_ERR_FILE --no-visualization
+                        argos3 -c $EXPERIMENT_FILE -l $LOG_FILE -e $LOG_ERR_FILE -z
                         # argos3 -c $EXPERIMENT_FILE --no-visualization
                         # argos3 -c $EXPERIMENT_FILE
 
@@ -232,68 +221,6 @@ do
         done
     done
 done
-
-### RUN one experiment
-
-######
-
-# # Parameters
-# run_num=6
-# print_count=""
-# seed=943500
-# team_num=4
-# worker_per_team=9
-# charger_per_team=1
-
-# k="$run_num"
-# i="$team_num"
-# j="$worker_per_team"
-# l="$charger_per_team"
-
-# # Set seed
-# sed -i "${LINE_SEED}s/.*/random_seed='$seed' \/>/" $EXPERIMENT_FILE
-
-# # Set run number
-# sed -i "${LINE_RUN_NUMBER}s/.*/run_number='$k'/" $EXPERIMENT_FILE
-
-# # Set team size
-# sed -i "${LINE_TEAM_SIZE}s/.*/<distribute_teams team_num='$i' robot_per_team='$j' charger_per_team='$l' center='0,0' density='0.2' worker_type='$WORKER_TYPE'\/>/" $EXPERIMENT_FILE
-
-# # Set log directory number
-# printf -v print_count "%03d" $k
-
-# # Run experiment
-# echo "Running experiment $count, No_teams = $i, robots_per_team = $j, chargers_per_team = $l, run = $k, (s = $seed) ..."
-# LOG_FILE="$RESULT_DIR/log.txt"
-# LOG_ERR_FILE="$RESULT_DIR/log_err.txt"
-
-# # echo $LOG_FILE
-
-# # Create folder if it doesn't exist
-# if [ ! -d "${RESULT_DIR}" ]; then
-#     mkdir -p "${RESULT_DIR}"
-#     echo "Created folder: ${RESULT_DIR}"
-# fi
-# argos3 -c $EXPERIMENT_FILE -l $LOG_FILE -e $LOG_ERR_FILE
-# # argos3 -c $EXPERIMENT_FILE
-# NEW_LOG_FILE="$RESULT_DIR/${EXPERIMENT_NAME}(${STRATEGY_TYPE})_${i}T_${j}R_${l}C_$print_count/log.txt"
-# NEW_LOG_ERR_FILE="$RESULT_DIR/${EXPERIMENT_NAME}(${STRATEGY_TYPE})_${i}T_${j}R_${l}C_$print_count/log_err.txt"
-# # echo $LOG_FILE
-# # echo $NEW_LOG_FILE
-# mv $LOG_FILE $NEW_LOG_FILE
-# mv $LOG_ERR_FILE $NEW_LOG_ERR_FILE
-# # echo "done"
-
-# file="$RESULT_DIR/${EXPERIMENT_NAME}(${STRATEGY_TYPE})_${i}T_${j}R_${l}C_$print_count/summary.csv"
-# # Get the specified lines from the CSV file
-# line1_num=9
-# # line2_num=11
-# line1=$(sed "${line1_num}q;d" ${file})
-# # line2=$(sed "${line2_num}q;d" ${file})
-# # Print the lines on the same line, with the first line in blue and the second line in green
-# echo -e "\033[32m${line1}\033[0m"
-
-# ######
 
 # End timing
 end=$(date +%s.%N)
